d273e292ade5863e992c246bf6ab598ef3216d01
Hildo <hyldo.costa@hotmail.com>
2026-05-24 20:55:26 -0300

    feat: implementa modelo Historico e atualiza para Prisma 7 com config.js

---
+++ prisma.config.js
@@ -0,0 +1,7 @@
+const prismaConfig = {
+  datasource: {
+    url: "postgresql://neondb_owner:npg_z6dgIwBn2SMi@ep-empty-thunder-acgqphq0.sa-east-1.aws.neon.tech/neondb?sslmode=require"
+  }
+};
+
+export default prismaConfig;
